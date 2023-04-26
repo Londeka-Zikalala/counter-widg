0fe162e82a80d98d535a4e253da6831674e8ae83
Londeka Zikalala <londekazikalala@gmail.com>
2023-04-26 16:38:00 +0200

    event listeners called

--- counter.js
+++ counter.js
@@ -1,7 +1,7 @@
 //references for all the elements
 const resetButton = document.querySelector('.reset');
 const counterDisplay = document.querySelector('.counter');
-const multipleInput = document.querySelector('.multiple');
+const multipleInputElem = document.querySelector('.multiple');
 const decrementBtn = document.querySelector('.decrement');
 const incrementBtn = document.querySelector('.increment');
 
@@ -9,12 +9,25 @@ const incrementBtn = document.querySelector('.increment');
 var counter = 0;
 var multiples = 1;
 //add event listeners 
-function decrementBtn(){
+
+function decrementBtnFunction(){
     counter -= multiples;
+    alert(counterDisplay = counter)
 }
 function incrementBtnFunction(){
     counter += multiples;
+   alert(counterDisplay = counter)
 }
 function resetBtnFunction(){
  counter = 0;
+ alert(counterDisplay = counter)
 }
+
+function inputText () {
+    multipleInputElem= Number(multipleInputElem.value);
+}
+
+decrementBtn.addEventListener('click', decrementBtnFunction);
+incrementBtn.addEventListener('click', incrementBtnFunction);
+resetButton.addEventListener('click', resetBtnFunction);
+multipleInputElem.addEventListener('click', inputText);
